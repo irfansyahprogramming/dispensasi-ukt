4136e9984bf487f6719f009fc20c146f6b13faff
Irfansyah <irfansyah1979@gmail.com>
2022-04-01 00:35:28 +0700

    proposal aplikasi

---
+++ resources/views/home.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Dispensasi UKT</title>
+  </head>
+  <body>
+    
+    <div class="text-center">
+        <img src="img/Logo-unj.png" alt="UNJ" width="400" height="400" class="img-fluid img-thumbnail rounded-circle my-3">
+        
+        <h1 class="mt-5 text-danger">
+            PENGEMBANGAN APLIKASI<br/>
+            PERMOHOHONAN KERINGANAN UANG KULIAH TUNGGAL<br/>
+            BAGI MAHASISWA YANG TERDAMPAK PANDEMI COVID 19<br/>
+            DI UNIVERSITAS NEGERI JAKARTA
+        </h1>
+
+    </div>
+    
+    
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </body>
+</html>
