0e857eda10d7c4fd88599a59a4a3e27b35817dd4
atropa-belladona <blackshields99@gmail.com>
2022-06-03 16:43:42 +0700

    Didin.2022.06.03 - jumlah pada dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,21 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\PengajuanDispensasiUKTModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 
 
 class HomeController extends Controller
 {
-    public function index(){
-        
-        if(!Session::has('isLoggedIn')){
+    public function index()
+    {
+
+        if (!Session::has('isLoggedIn')) {
             return redirect()->to('login');
         }
 
-        $user = session ('user_name');
-        $mode = session ('user_mode');
-        
+        $user = session('user_name');
+        $mode = session('user_mode');
+
         $arrData = [
             'title'         => 'Home',
             'active'        => 'home',
@@ -26,12 +28,12 @@ class HomeController extends Controller
             'home_active'   => 'active',
             'dispen_active' => '',
             'penerima_active'   => '',
-            'laporan_active' => ''
+            'laporan_active' => '',
+            'pengajuan' => collect(PengajuanDispensasiUKTModel::where('kode_prodi', 'like', trim(session('user_unit')) . '%')->get()),
             // 'posts' => Post::All()
             //'posts' => Post::latest()->get()
         ];
-        
-        return view('home',$arrData);
-    }
 
+        return view('home', $arrData);
+    }
 }
