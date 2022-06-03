ac0ca92af50439053c4809d9a0c3fca52f418ebb
Irfansyah <irfansyah1979@gmail.com>
2022-06-03 14:49:18 +0700

    isi-log

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -10,45 +10,74 @@ use Illuminate\Support\Facades\Session;
 
 class LaporanController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        if(!Session::has('isLoggedIn')){
+        if (!Session::has('isLoggedIn')) {
             return redirect()->to('login');
         }
-                
-        $periode = BukaDispensasi::checkOpenPeriode();
-        
-        $user = session ('user_name');
-        $mode = session ('user_mode');
-        $unit = session ('user_unit');
-        
-        $periode = BukaDispensasi::where('aktif','1')->first();
-        if ($periode){
+
+        $user = session('user_name');
+        $mode = session('user_mode');
+        $unit = session('user_unit');
+
+        $periode = BukaDispensasi::where('aktif', '1')->first();
+        if ($periode) {
             $tombol = "";
             $semester = $periode->semester;
-        }else{
+        } else {
             $tombol = "disabled";
             $semester = "";
         }
 
-        $pengajuan = DB::table('tb_pengajuan_dispensasi')
-        ->where('kode_prodi','like',trim(session('user_unit')).'%')
-        ->where('semester',trim($semester))
-        ->get();
+        $pengajuan = DB::table('tb_pengajuan_dispensasi');
+        // ->where('kode_prodi','like',trim(session('user_unit')).'%')
+        // ->where('semester',trim($semester))
+
+        // for filtering
+        // by semester
+        if (isset($request->semester) and $request->semester != 'All') {
+            $pengajuan = $pengajuan->where('semester', trim($request->semester));
+        } else {
+            $pengajuan = $pengajuan->where('semester', trim($semester));
+        }
+
+        // by prodi
+        if (isset($request->prodi) and $request->prodi != 'All') {
+            $pengajuan = $pengajuan->where('kode_prodi', trim($request->prodi));
+        } else {
+            $pengajuan = $pengajuan->where('kode_prodi', 'like', trim(session('user_unit')) . '%');
+        }
+
+        // by jenis pengajuan
+        if (isset($request->jenis) and $request->jenis != 'All') {
+            $pengajuan = $pengajuan->where('jenis_dispensasi', $request->jenis);
+        }
+
+        // by status pengajuan
+        if (isset($request->status) and $request->status != 'All') {
+            $pengajuan = $pengajuan->where('status_pengajuan', $request->status);
+        }
+
+        // get data pengajuan
+        $pengajuan = $pengajuan->get();
+        
+        // flash request data
+        $request->flash();
+
 
         $listSemester = DB::table('ref_periode')->get();
         $listJenis = DB::table('ref_jenisdipensasi')->get();
         $listStatus = DB::table('ref_status_pengajuan')->get();
 
         // get mengajar from siakad
-        $url = "http://103.8.12.212:36880/siakad_api/api/as400/programStudi/". trim(session ('user_unit'));
+        $url = "http://103.8.12.212:36880/siakad_api/api/as400/programStudi/" . trim(session('user_unit'));
         //echo $url;
         $response = Http::get($url);
         $listProdi = json_decode($response);
         //var_dump($listProdi);
 
-        foreach($pengajuan as $ajuan){
-            $ajuan->nom_ukt = number_format($ajuan->nominal_ukt,0);
+        foreach ($pengajuan as $ajuan) {
+            $ajuan->nom_ukt = number_format($ajuan->nominal_ukt, 0);
             $ajuan->jenis = DB::table('ref_jenisdipensasi')->where('id', $ajuan->jenis_dispensasi)->first()->jenis_dispensasi;
             $ajuan->status = DB::table('ref_status_pengajuan')->where('id', $ajuan->status_pengajuan)->first()->status_ajuan;
             $ajuan->kelompok = DB::table('ref_kelompok_ukt')->where('id', $ajuan->kelompok_ukt)->first()->kelompok;
@@ -74,7 +103,7 @@ class LaporanController extends Controller
             'pengajuan'         => $pengajuan
         ];
 
-        return view('laporan.index',$arrData);
+        return view('laporan.index', $arrData);
         //return view('pengajuan_dispensasi',compact('list_dispensasi','kel_ukt','pengajuan','subtitle'));
     }
 }
--- resources/views/laporan/index.blade.php
+++ resources/views/laporan/index.blade.php
@@ -1,173 +1,177 @@
 @extends('layouts.main')
 
 @section('style')
-    <link rel="stylesheet" href="{{ asset('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
 @endsection
 
 @section('contain')
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>{{ $subtitle }}</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="/home">{{ $title }}</a></li>
-              <li class="breadcrumb-item active">{{ $subtitle }}</li>
-            </ol>
-          </div>
+  <section class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1>{{ $subtitle }}</h1>
+        </div>
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="/home">{{ $title }}</a></li>
+            <li class="breadcrumb-item active">{{ $subtitle }}</li>
+          </ol>
         </div>
       </div>
-    </section>
+    </div>
+  </section>
 
-    <section class="content">
-     <div class="card">
-        <div class="card-header">
-          <h3 class="card-title">Laporan Dispensasi UKT Semester {{ $semester }}</h3>
+  <section class="content">
+    <div class="card">
+      <div class="card-header">
+        <h3 class="card-title">Laporan Dispensasi UKT Semester {{ $semester }}</h3>
 
-          <div class="card-tools">
-            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-              <i class="fas fa-minus"></i>
+        <div class="card-tools">
+          <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+            <i class="fas fa-minus"></i>
+          </button>
+          <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+            <i class="fas fa-times"></i>
+          </button>
+        </div>
+      </div>
+      <div class="card-body">
+        <div class="mt-4">
+          <button class="btn btn-outline-primary" id="btnFilter" data-toggle="modal" data-target="#modal-FilterData"><i class="ace-icon fa fa-list"></i> Filter</button>
+        </div>
+
+        <div class="mt-4 table-responsive">
+          <table id="dataTabel" class="table table-hover">
+            <thead>
+              <tr>
+                <th scope="col">No</th>
+                <th scope="col">NIM</th>
+                <th scope="col">Nama</th>
+                <th scope="col">Program Studi</th>
+                <th scope="col">Jenis Dispensasi</th>
+                <th scope="col">Status Pengajuan Dispensasi</th>
+              </tr>
+            </thead>
+            <tbody>
+              @foreach ($pengajuan as $item)
+                <tr>
+                  <td>{{ $loop->iteration }}</td>
+                  <td>{{ $item->nim }}</td>
+                  <td>{{ $item->nama }}</td>
+                  <td>{{ $item->jenjang_prodi }} {{ $item->nama_prodi }}</td>
+                  <td>{{ $item->jenis }}</td>
+                  <td>{{ $item->status ?? '' }}</td>
+                </tr>
+              @endforeach
+
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div class="card-footer">
+      </div>
+    </div>
+
+    <div id="modal-FilterData" class="modal fade" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog">
+      <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+          <div class="modal-header py-2">
+            <h5 class="modal-title">Filter Daftar Dispensasi UKT</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">×</span>
             </button>
-            <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-              <i class="fas fa-times"></i>
-            </button>
           </div>
-        </div>
-        <div class="card-body">
-            <div class="mt-4">
-                <button class="btn btn-outline-primary" id="btnFilter" data-toggle="modal" data-target="#modal-FilterData"><i class="ace-icon fa fa-list"></i> Filter</button>
-            </div>
+          <div class="modal-body">
+            <form id="filter-laporan-pengajuan" action="{{ route('laporan.index') }}" method="GET">
+              <div class="modal-body py-2">
+                <div class="form-body">
+                  <div class="form-group">
+                    <label for="semester">Semester</label>
+                    <select class="form-control" id="semester" name="semester">
+                      <option value="All">Semua Semester</option>
+                      @foreach ($listSemester as $sms)
+                        <option value="{{ $sms->semester }}" {{ $sms->id == old('semester') ? 'selected' : '' }}>{{ $sms->semester }} / {{ $sms->des_semester }}</option>
+                      @endforeach
+                    </select>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="prodi">Program Studi</label>
+                  <select class="form-control" id="prodi" name="prodi">
+                    <option value="All">Semua Prodi</option>
+                    @foreach ($listProdi->isi as $prd)
+                      <option value="{{ $prd->kodeProdi }}" {{ $prd->kodeProdi == old('prodi') ? 'selected' : '' }}>{{ $prd->jenjangProdi }} {{ $prd->namaProdi }}</option>
+                    @endforeach
+                  </select>
+                </div>
+                <div class="form-group">
+                  <label for="jenis">Jenis Dispensasi</label>
+                  <select class="form-control" id="jenis" name="jenis">
+                    <option value="All">Semua Jenis Dispensasi</option>
+                    @foreach ($listJenis as $jns)
+                      <option value="{{ $jns->id }}" {{ $jns->id == old('jenis') ? 'selected' : '' }}>{{ $jns->jenis_dispensasi }}</option>
+                    @endforeach
+                  </select>
+                </div>
+                <div class="form-group">
+                  <label for="status">Status Pengajuan</label>
+                  <select class="form-control" id="status" name="status">
+                    <option value="All">Semua Status Pengajuan Dispensasi</option>
+                    @foreach ($listStatus as $sts)
+                      <option value="{{ $sts->id }}" {{ $sts->id == old('status') ? 'selected' : '' }}>{{ $sts->status_ajuan }}</option>
+                    @endforeach
+                  </select>
+                </div>
+              </div>
+            </form>
 
-            <div class="mt-4 table-responsive">
-                <table id="dataTabel" class="table table-hover">
-                    <thead>
-                    <tr>
-                        <th scope="col">No</th>
-                        <th scope="col">NIM</th>
-                        <th scope="col">Nama</th>
-                        <th scope="col">Program Studi</th>
-                        <th scope="col">Jenis Dispensasi</th>
-                        <th scope="col">Status Pengajuan Dispensasi</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($pengajuan as $item)
-                        <tr>
-                            <td>{{ $loop->iteration }}</td>
-                            <td>{{ $item->nim }}</td>
-                            <td>{{ $item->nama }}</td>
-                            <td>{{ $item->jenjang_prodi }} {{ $item->nama_prodi }}</td>
-                            <td>{{ $item->jenis }}</td>
-                            <td>{{ $item->status ?? '' }}</td>
-                        </tr>
-                        @endforeach
-                    
-                    </tbody>
-                </table>
-            </div>
-        </div>
-        <div class="card-footer">
-        </div>
-      </div>
-
-      <div id="modal-FilterData" class="modal fade" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog">
-        <div class="modal-dialog modal-xl">
-          <div class="modal-content">
-            <div class="modal-header py-2">
-                <h5 class="modal-title">Filter Daftar Dispensasi UKT</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">×</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <div class="modal-body py-2">  
-                    <div class="form-body">
-                        <div class="form-group">
-                            <label for="semester">Semester</label>
-                            <select class="form-control" id="semester" name="semester">
-                                <option value="All">Semua Semester</option>
-                                @foreach ($listSemester as $sms)
-                                    <option value="{{ $sms->semester }}">{{ $sms->semester }} / {{ $sms->des_semester }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="prodi">Program Studi</label>
-                        <select class="form-control" id="prodi" name="prodi">
-                            <option value="All">Semua Prodi</option>
-                            @foreach ($listProdi->isi as $prd)
-                                <option value="{{ $prd->kodeProdi }}">{{ $prd->jenjangProdi }} {{ $prd->namaProdi }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label for="jenis">Jenis Dispensasi</label>
-                        <select class="form-control" id="jenis" name="jenis">
-                            <option value="All">Semua Jenis Dispensasi</option>
-                            @foreach ($listJenis as $jns)
-                                <option value="{{ $jns->id }}">{{ $jns->jenis_dispensasi }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label for="status">Status Pengajuan</label>
-                        <select class="form-control" id="status" name="status">
-                            <option value="All">Semua Status Pengajuan Dispensasi</option>
-                            @foreach ($listStatus as $sts)
-                                <option value="{{ $sts->id }}">{{ $sts->status_ajuan }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                </div>    
-            </div>
-            <div class="modal-footer py-2">
-                <button type="button" class="btn btn-primary" onclick="cetakPenerima()"><i class="fas fa-solid fa-filter"></i> Filter</button>
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
-            </div>
+          </div>
+          <div class="modal-footer py-2">
+            <button type="button" class="btn btn-primary" onclick="document.getElementById('filter-laporan-pengajuan').submit();"><i class="fas fa-solid fa-filter"></i> Filter</button>
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
           </div>
         </div>
       </div>
-    </section>
+    </div>
+  </section>
 @endsection
 
-@section ('script')
-<script src="{{ asset('plugins/datatables/jquery.dataTables.min.js') }}"></script>
-<script src="{{ asset('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
-<script src="{{ asset('plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
-<script src="{{ asset('plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
-<script src="{{ asset('plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
-<script src="{{ asset('plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
-<script src="{{ asset('plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
-<script src="{{ asset('plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
-<script src="{{ asset('plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
-<script>
+@section('script')
+  <script src="{{ asset('plugins/datatables/jquery.dataTables.min.js') }}"></script>
+  <script src="{{ asset('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+  <script src="{{ asset('plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+  <script src="{{ asset('plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+  <script src="{{ asset('plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
+  <script src="{{ asset('plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+  <script src="{{ asset('plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
+  <script src="{{ asset('plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
+  <script src="{{ asset('plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
+  <script>
+    $(function() {
 
-    $(function () {
-      
-        $("#dataTabel1").DataTable({
-            "responsive": true, "lengthChange": false, "autoWidth": false,
-            "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
-        }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+      $("#dataTabel1").DataTable({
+        "responsive": true,
+        "lengthChange": false,
+        "autoWidth": false,
+        "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+      }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
 
-        $('#dataTabel').DataTable({
-            "paging": true,
-            "lengthChange": false,
-            "searching": false,
-            "ordering": true,
-            "info": true,
-            "autoWidth": false,
-            "responsive": true,
-        });
+      $('#dataTabel').DataTable({
+        "paging": true,
+        "lengthChange": false,
+        "searching": false,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false,
+        "responsive": true,
+      });
     });
-    
-    function cetakPenerima(){
-        var semester = $('#semester').val();
-        alert(semester);
-    }
-</script>
+
+    // function cetakPenerima(){
+    //     var semester = $('#semester').val();
+    //     alert(semester);
+    // }
+  </script>
 @endsection
