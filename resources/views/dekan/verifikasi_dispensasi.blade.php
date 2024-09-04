66b83ab4b3b80ffd22e6446daffd4170572d3397
irfansyah <irfansyah1979@gmail.com>
2024-09-04 11:53:29 +0700

    Update Dispensasi 040920241153

--- resources/views/dekan/verifikasi_dispensasi.blade.php
+++ resources/views/dekan/verifikasi_dispensasi.blade.php
@@ -391,13 +391,20 @@
       }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
 
       $('#dataTabel').DataTable({
-        "paging": true,
-        "lengthChange": false,
-        "searching": false,
-        "ordering": true,
-        "info": true,
-        "autoWidth": false,
+        // "paging": false,
+        // "lengthChange": false,
+        "searching": true,
+        // "ordering": true,
+        // "info": true,
+        // "autoWidth": false,
         "responsive": true,
+        "layout": {
+          "bottomEnd": {
+              "paging": {
+                  "type": 'simple'
+              }
+          }
+        }
       });
     });
 
