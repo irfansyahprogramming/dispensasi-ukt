ef6ae285b67747b2c398ccdb9fe7f9b85a0c2da4
irfansyah <irfansyah1979@gmail.com>
2024-09-09 09:54:41 +0700

    Update Dispensasi 090920240954

--- resources/views/wr2/verifikasi_dispensasi.blade.php
+++ resources/views/wr2/verifikasi_dispensasi.blade.php
@@ -378,6 +378,9 @@
   <script src="{{ asset('plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
   <script src="{{ asset('plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
   <script src="{{ asset('plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+  <script src="{{ asset('plugins/jszip/jszip.min.js') }}"></script>
+  <script src="{{ asset('plugins/pdfmake/pdfmake.min.js') }}"></script>
+  <script src="{{ asset('plugins/pdfmake/vfs_fonts.js') }}"></script>
   <script src="{{ asset('plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
   <script src="{{ asset('plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
   <script src="{{ asset('plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
@@ -394,14 +397,22 @@
       }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
 
       $('#dataTabel').DataTable({
-        "paging": true,
-        "lengthChange": false,
-        "searching": false,
-        "ordering": true,
-        "info": true,
-        "autoWidth": false,
+        // "paging": false,
+        // "lengthChange": false,
+        "searching": true,
+        // "ordering": true,
+        // "info": true,
+        // "autoWidth": false,
         "responsive": true,
-      });
+        "buttons": ["copy", "csv", "excel", "pdf", "print"],
+        "layout": {
+          "bottomEnd": {
+              "paging": {
+                  "type": 'simple'
+              }
+          }
+        }
+      }).buttons().container().appendTo('#dataTabel_wrapper .col-md-6:eq(0)');
     });
 
     function do_this() {
