604e92786cbb53b10eeef211962de4eccc32f372
irfansyah <irfansyah1979@gmail.com>
2026-07-24 18:12:36 +0700

    trf apps 24072026-6

--- app/Http/Controllers/VerifikasiUKTController.php
+++ app/Http/Controllers/VerifikasiUKTController.php
@@ -48,9 +48,13 @@ class VerifikasiUKTController extends Controller
 
         $pengajuan = DB::table('tb_pengajuan_dispensasi')
             ->select('tb_pengajuan_dispensasi.id', 'tb_pengajuan_dispensasi.semester', 'tb_pengajuan_dispensasi.nim', 'tb_pengajuan_dispensasi.nama', 'tb_pengajuan_dispensasi.kode_prodi', 'tb_pengajuan_dispensasi.nama_prodi', 'tb_pengajuan_dispensasi.jenjang_prodi', 'tb_pengajuan_dispensasi.kelompok_ukt', 'tb_pengajuan_dispensasi.nominal_ukt','tb_pengajuan_dispensasi.alamat', 'tb_pengajuan_dispensasi.no_hp','tb_pengajuan_dispensasi.email','tb_pengajuan_dispensasi.pekerjaan', 'tb_pengajuan_dispensasi.jabatan_kerja','tb_pengajuan_dispensasi.pengalihan', 'tb_pengajuan_dispensasi.awal_pengajuan','tb_pengajuan_dispensasi.status_pengajuan', 'tb_pengajuan_dispensasi.semesterke','tb_pengajuan_dispensasi.sks_belum', 'tb_pengajuan_dispensasi.file_pernyataan','tb_pengajuan_dispensasi.file_keterangan', 'tb_pengajuan_dispensasi.file_permohonan','tb_pengajuan_dispensasi.file_penghasilan', 'tb_pengajuan_dispensasi.file_phk','tb_pengajuan_dispensasi.file_pailit', 'tb_pengajuan_dispensasi.file_pratranskrip','tb_pengajuan_dispensasi.potongan' ,'tb_pengajuan_dispensasi.ditagihkan', 'tb_pengajuan_dispensasi.angsuran1', 'tb_pengajuan_dispensasi.angsuran2', 'tb_pengajuan_dispensasi.kel_ukt_baru', 'ref_jenisdipensasi.jenis_dispensasi', 'ref_status_pengajuan.status_ajuan', 'ref_kelompok_ukt.kelompok')
-            ->where('status_pengajuan', [1,2,3,4,6,7,22,23,24,25,26])
-            ->where('kode_prodi', 'like', trim(session('user_unit')) . '%');
-            
+            ->where('tb_pengajuan_dispensasi.kode_prodi', 'like', trim(session('user_unit')) . '%')
+            ->whereIn('tb_pengajuan_dispensasi.status_pengajuan',['1','2','3','4','5','6','7','22'])
+            // ->WhereBetween('tb_pengajuan_dispensasi.status_pengajuan',['22','26'])
+            // ->where('tb_pengajuan_dispensasi.status_pengajuan','>=','1')
+            // ->where('tb_pengajuan_dispensasi.status_pengajuan','<=','7')
+                ;
+        // @dd($pengajuan);
         if (isset($request->semester) and $request->semester != 'All') {
             $pengajuan = $pengajuan->where('semester', trim($request->semester));
         } else {
--- resources/views/verifikasi_dispensasi.blade.php
+++ resources/views/verifikasi_dispensasi.blade.php
@@ -119,7 +119,7 @@
                   <td class="text-center">
                     @if ($item->status_pengajuan == 2 || $item->status_pengajuan == 22)
                       <button type="button" data-toggle="tooltip" data-placement="top" title="Verifikasi Data" class="btn btn-outline-success" onclick="verifData({{ $item->id }})"><i class="fas fa-edit"></i> Edit Status </button>
-                    @elseif ($item->status_pengajuan > 3)
+                    @elseif ($item->status_pengajuan > 2)
                       <button type="button" class="btn btn-outline-success"></i> Lock </button>
                     @else
                       <button type="button" data-toggle="tooltip" data-placement="top" title="Verifikasi Data" class="btn btn-outline-info" onclick="verifData({{ $item->id }})"><i class="fas fa-edit"></i> Proses</button>
